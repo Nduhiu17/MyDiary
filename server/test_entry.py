609c3e16bf4c3c92ac9ca01b9c4c57903ab03fbc
Your Name <nduhiu254@gmail.com>
2018-07-16 19:35:14 +0300

    Add test_entry.py file and its coresponding model method

---
+++ server/test_entry.py
@@ -0,0 +1,19 @@
+import unittest
+from models import Entry
+
+class TestEntry(unittest.TestCase):
+    def tearDown(self):
+        Entry.entries = []
+
+    def test_entry_init(self):
+        new_entry = Entry(title="graduation ceremony", description="it was nice attending.",date_created=" ")
+        self.assertEqual(new_entry.title, "graduation ceremony")
+
+    def setUp(self):
+        self.new_entry = Entry(title="graduation ceremony",description="it was nice attending",date_created=" ")
+        self.new_entry.save()
+
+    def test_save(self):
+        new_entry = self.new_entry
+        new_entry.save()
+        self.assertEqual(len(Entry.entries), 2)
