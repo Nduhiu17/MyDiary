484a651707b85d3f43c6b4c211b962af92376460
Your Name <nduhiu254@gmail.com>
2018-08-02 23:54:11 +0300

    Update requirements.txt and start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL="dbname='dfnto48h4ufbi7' user='jkxwwyumvnralw' host='ec2-50-19-86-139.compute-1.amazonaws.com' password='7da5145d4d847858d077725513fb772ce186f8f263e7e203bc9ffb277619465e'"
+export DATABASE_URL="postgres://jkxwwyumvnralw:7da5145d4d847858d077725513fb772ce186f8f263e7e203bc9ffb277619465e@ec2-50-19-86-139.compute-1.amazonaws.com:5432/dfnto48h4ufbi7"
 
 
 export SECRET_KEY='toomanysecrets'
